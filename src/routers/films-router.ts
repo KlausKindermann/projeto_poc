575cd73e2b0df89c47654bf86a784884aed2b923
klaus Kindermann <klauskindermann94@gmail.com>
2023-07-03 16:41:04 -0300

    teste3

--- src/routers/films-router.ts
+++ src/routers/films-router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import * as filmsController from "../controllers/films-controller"
+import filmSchema from "../schemas/film-schema";
+import { validateSchema } from "../midlewares/schema-validator";
+
 
 const filmsRouter = Router();
 
 filmsRouter.get("/films", filmsController.getFilms);
-filmsRouter.post("/film", filmsController.createFilm);
+filmsRouter.post("/film", validateSchema(filmSchema), filmsController.createFilm);
 
 export default filmsRouter;
